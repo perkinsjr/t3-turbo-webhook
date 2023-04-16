fde2f0c49fd6a5a006c4ec37f928749ae19f3453
James Perkins <jamesperkins@hey.com>
2023-04-16 08:37:39 -0400

    add webhook endpoint to test

---
+++ apps/nextjs/src/pages/api/webhook/create-user.ts
@@ -0,0 +1,4 @@
+import { NextApiRequest, NextApiResponse } from "next";
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ message: "it works" });
+}
